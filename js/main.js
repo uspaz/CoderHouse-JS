a9889a06742a553cc4f0aa0e4d6d98e0cca173cc
uspaz <Matias-Teijeiro2015@hotmail.com>
2022-04-30 17:50:35 -0300

    Entrega Final

--- js/main.js
+++ js/main.js
@@ -1,64 +1,90 @@
 // Variables, Arrays y acceso al DOM 
+const listaPokemons = [];
+const spinner = document.querySelector(".spinner");
+const containerPokemons = document.getElementById("pokemons");
+const pages = document.querySelector(".pages");
 let usuario = document.getElementById("usuario");
 let offset = 1;
 let limit = 12;
 
-/* Hago llamado a la API y guardo sus datos dependiendo de un id para que vengan de a 1 y voy creando su estructura para mostrarlos */
-async function getPokemons(id) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-  const pokemons = await res.json();
-  showPokemons(pokemons);
-}
+/* Hago llamado a la API y guardo sus datos dependiendo de un id para que vengan de a uno, guardarlos en una variable para luego pushear esos datos al array y luego utilizar otra función para mostrarlos con su estructura */
 
-
-/* Muestro los pokemons utilizando otra función que al traer un pokemon con su id (que se define gracias al indice del for y delimitado por las variables) */
-
-function paginacion() {
-  if( offset >= 1){
-    for (let i = offset; i <= limit; i++) {
-      getPokemons(i);
-    }
+async function getPokemons() {
+  for (let i = offset; i <= limit; i++) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    const pokemons = await res.json();
+    listaPokemons.push(pokemons);    
   }
+  hideSpinner();
+  showPokemons(listaPokemons);
 }
 
+/* Hago que evalue si hay algun pokemon en el array (en caso de que haya los saco), y luego utilizo una condición para saber si dentro de los limites de los pokemons que hay, para luego llamar a otra función para poder mostrarlos  */
+
+function pagPokemons() {
+  listaPokemons.splice(0, listaPokemons.length);
+  showSpinner();
+
+  setTimeout(()=>{
+    if( (offset >= 1) || (limit <= 898)){
+      getPokemons();
+    }
+  }, 1000);
+}
+
+/* Botón para avanzar a los siguientes 12 pokemons */
 document.getElementById("siguiente").addEventListener("click", ()=>{
   offset += 12;
   limit += 12;
-  paginacion();
+  pagPokemons();
 });
 
-
+/* Botón para retroceder a los anteriores 12 pokemons */
 document.getElementById("anterior").addEventListener("click", ()=>{
   offset -= 12;
   limit -= 12;
-  paginacion();
+  pagPokemons();
 });
 
+/* Botón para avanzar hasta los ultimos 12 pokemons */
+document.getElementById("final").addEventListener("click", ()=>{
+  offset = 887;
+  limit = 898;
+  pagPokemons();
+});
 
-/* Realizo la estructura de como quiero que se vean los pokemons que tengo según la cantidad de elementos que utilice */
-function showPokemons(pokemon){
+/* Botón para retroceder a los primeros 12 pokemons */
+document.getElementById("principal").addEventListener("click", ()=>{
+  offset = 1;
+  limit = 12;
+  pagPokemons();
+});
+
+/* Creo la estrutura para los pokemons que tengo según la cantidad de elementos que utilice */
+function showPokemons(lista){
   let container = "";
+  lista.forEach( (pokemon) => {
 
-  let {id, name , types} = pokemon;
-  if(types.length <= 1){
-    container += 
-      `<div class="card">
-            <figure class="card--image">
-              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
-            </figure>
-            <div class="card--data">
-              <p>#${id.toString().padStart(3, '0')}</p>
-              <h2>${name}</h2>
-            </div>
-            <div class="card--type">
-              <span class="${(pokemon.types[0].type.name)}">${pokemon.types[0].type.name}</span>
-            </div>
-        </div>`
-  }else{
-    container += 
+    let {id, name , types} = pokemon;  
+    if(types.length <= 1){
+      container += 
       `<div class="card">
           <figure class="card--image">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="" lazy="loading">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
+          </figure>
+          <div class="card--data">
+            <p>#${id.toString().padStart(3, '0')}</p>
+            <h2>${name}</h2>
+          </div>
+          <div class="card--type">
+            <span class="${(types[0].type.name)}">${types[0].type.name}</span>
+          </div>
+        </div>`
+    }else{
+      container += 
+      `<div class="card">
+          <figure class="card--image">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="">
           </figure>
           <div class="card--data">
           <p>#${id.toString().padStart(3, '0')}</p>
@@ -69,10 +95,61 @@ function showPokemons(pokemon){
             <span class="${(types[1].type.name)}">${types[1].type.name}</span>
           </div>
         </div>`;
-  }
-   document.getElementById("pokemons").innerHTML += container;
+    }
+    containerPokemons.innerHTML = container;
+  });
 }
 
+/* En esta función agrupo los filtrados por generacion*/
+function genPokemons() {
+  document.getElementById("gen1").addEventListener("click", ()=>{
+    offset = 1;
+    limit = 151;
+    pagPokemons();
+  });
+
+  document.getElementById("gen2").addEventListener("click", ()=>{
+    offset = 152;
+    limit = 251;
+    pagPokemons();
+  });
+
+  document.getElementById("gen3").addEventListener("click", ()=>{
+    offset = 252;
+    limit = 386;
+    pagPokemons();
+  });
+
+  document.getElementById("gen4").addEventListener("click", ()=>{
+    offset = 387;
+    limit = 493;
+    pagPokemons();
+  });
+
+  document.getElementById("gen5").addEventListener("click", ()=>{
+    offset = 494;
+    limit = 649;
+    pagPokemons();
+  });
+
+  document.getElementById("gen6").addEventListener("click", ()=>{
+    offset = 650;
+    limit = 721;
+    pagPokemons();
+  });
+
+  document.getElementById("gen7").addEventListener("click", ()=>{
+    offset = 722;
+    limit = 809;
+    pagPokemons();
+  });
+
+  document.getElementById("gen8").addEventListener("click", ()=>{
+    offset = 810;
+    limit = 898;
+    pagPokemons();
+  });
+}
 
 // Agrego y quito los filtros cuando esta en vista mobile para que el usuario los pueda visualizar
 function mostrarFiltros(){
@@ -124,21 +201,34 @@ function signOut(){
   });
 }
 
+/* Filtro por los nombres de los pokemons, tiene mejor ultilidad cuando utilizamos los botones de generaciones porque hay una busqueda más amplia */
 function busqueda(){
   let search = document.getElementById("buscar").value;
   
   let listaFiltrada = listaPokemons.filter(element =>{
-    return element.nombre.toLowerCase().indexOf(search.toLowerCase()) > -1
+    return (element.name).toLowerCase().indexOf(search.toLowerCase()) > -1
   });
-  mostrarPokemons(listaFiltrada);
+  showPokemons(listaFiltrada);
 }
 document.getElementById("buscar").addEventListener("keyup", ()=>{
   busqueda();
 })
 
+function showSpinner() {
+  spinner.classList.add("spinner");
+  containerPokemons.style.display = "none";
+  pages.style.display = "none";
+}
+
+function hideSpinner() {
+  spinner.classList.remove("spinner");
+  containerPokemons.style.display = "flex";
+  pages.style.display = "flex";
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  paginacion();
+  pagPokemons();
+  genPokemons();
   mostrarFiltros();
   recuperarUsuario();
   signOut();
